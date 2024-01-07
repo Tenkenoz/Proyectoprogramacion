d0160da55a45a8bb99acd5bb158c58d5e1a12600
Tenkenoz <epobando1@espe.edu.ec>
2024-01-07 17:45:27 -0500

    Aporte de Alexander Ñacato Etapa,contrareloj,equipo,ciclista, montañaspridfsdfsadasd

--- src/proyectoprogramacion/CarreraCiclismo.java
+++ src/proyectoprogramacion/CarreraCiclismo.java
@@ -14,7 +14,7 @@ public class CarreraCiclismo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundio");
+        System.out.println("Hola munsdasddio");
     }
     
 }
